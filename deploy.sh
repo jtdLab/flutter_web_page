eb27ec7316a8a199db2af1dfbc49bab42bf528bf
jtdLab <72231111+jtdLab@users.noreply.github.com>
2023-03-05 22:11:40 +0100

    update build step in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # build
-flutter build web
+flutter build web --release --base-href /flutter_web_page/
 
 # deploy
 git add build/web -f
@@ -9,3 +9,4 @@ git add build/web -f
 git commit -m 'deploy'
 
 git subtree push --prefix build/web origin gh-pages
+
